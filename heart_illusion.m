5f2959801c9e557bddde034caf3123de9e068975
Shirley <shirley_dong@brown.edu>
2021-03-04 13:27:00 -0800

    stack heart matrix and background but can't figure out transparency

--- heart_illusion.m
+++ heart_illusion.m
@@ -2,31 +2,37 @@
 %% written on March 1, 2021
 %% by Haley Seo and Shirley Dong
 %% This script creates heart illusion
-% heart = imread('heart.jpg');
-% % read in heart image
-% %imshow(heart);
-% 
-% flipped_heart = imrotate(heart, 180);
-% %imshow(flipped_heart);
-% 
-% top_mat = repmat(heart,6,18);
-% % top half of matrix
-% 
-% flip_mat = repmat(flipped_heart,4,3);
-% % bottom corners of matrix, regions where red hearts are upside down
-% 
-% mid_mat = repmat(heart,4,12);
-% % bottom middle of matrix
-% 
-% bot_mat = [flip_mat mid_mat flip_mat];
-% % combine bottom sections of matrix
-% heart_mat = [top_mat;bot_mat];
-% % combine top and bottom of matrix
-% imshow(heart_mat);
+heart = imread('heart.png');
+% read in heart image
+imshow(heart);
+
+flipped_heart = imrotate(heart, 180);
+%imshow(flipped_heart);
+
+top_mat = repmat(heart,6,18);
+% top half of matrix
+
+flip_mat = repmat(flipped_heart,4,3);
+% bottom corners of matrix, regions where red hearts are upside down
+
+mid_mat = repmat(heart,4,12);
+% bottom middle of matrix
+
+bot_mat = [flip_mat mid_mat flip_mat];
+% combine bottom sections of matrix
+heart_mat = [top_mat;bot_mat];
+% combine top and bottom of matrix
+%imshow(heart_mat);
 
 %making the green background
 my_background = zeros(3050,6120,3);
 my_background(:,:,1)=0.83137;
 my_background(:,:,2)=0.8549;
 my_background(:,:,3)=0.17647;
-imshow(my_background)
+%imshow(my_background);
+
+% stacking heart_mat and my_background
+green_bg = image(my_background);
+axis off
+hold on
+hearts = image(heart_mat,'XData',[1 6120],'YData',[1 3050]);
